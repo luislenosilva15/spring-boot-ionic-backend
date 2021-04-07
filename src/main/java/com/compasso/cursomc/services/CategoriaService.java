2033f0932c8b6a8a08eb3acced3843263cb3816b
Luis Leno <luizleno15@gmail.com>
2021-04-07 11:33:15 -0300

    Ajuste endpoint

--- src/main/java/com/compasso/cursomc/services/CategoriaService.java
+++ src/main/java/com/compasso/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.compasso.cursomc.domain.Categoria;
 import com.compasso.cursomc.repositories.CategoriaRepository;
+import com.compasso.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -15,7 +16,9 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) { 
 		 Optional<Categoria> obj = repo.findById(id); 
-		return obj.orElse(null); 
+		 return obj.orElseThrow(() -> new ObjectNotFoundException( 
+				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName())); 
 		} 
-
 }
+
+
