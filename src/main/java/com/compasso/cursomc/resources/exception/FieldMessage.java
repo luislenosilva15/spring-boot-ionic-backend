d9fadfb7c874c65808222e2fbf8f9917b4eb8017
luis <45435291+luislenosilva15@users.noreply.github.com>
2021-04-16 08:40:55 -0300

    Personalizando o retorno da validação

---
+++ src/main/java/com/compasso/cursomc/resources/exception/FieldMessage.java
@@ -0,0 +1,40 @@
+package com.compasso.cursomc.resources.exception;
+
+import java.io.Serializable;
+
+public class FieldMessage implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String fieldName;
+	private String message;
+	
+	public FieldMessage(){
+
+}
+	public FieldMessage(String fieldName, String message) {
+		super();
+		this.setFieldName(fieldName);
+		this.setMessage(message);
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getFieldName() {
+		return fieldName;
+	}
+
+	public void setFieldName(String fieldName) {
+		this.fieldName = fieldName;
+	}
+	
+	
+	
+}
+
+	
